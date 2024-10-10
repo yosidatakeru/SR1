274aa164db1c4deb3d6e2d21906f94510421b3fe
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-10-10 15:05:49 +0900

    プロジェクト ファイルを追加します。

---
+++ DogTest/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Dog
+    {
+        public void Bark() 
+        {
+            //printf的な奴
+            Console.WriteLine("ガウガウ");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("わんわん");
+        }
+    }
+}
---
+++ DogTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog hachi = new Dog();
+            hachi.Bark();
+
+            Dog pochi = new Dog();
+            pochi.Bark();
+            pochi.Run();
+            Console.ReadLine();
+
+
+        }
+    }
+}
